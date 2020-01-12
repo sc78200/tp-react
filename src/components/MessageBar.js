01f6c31bc6b2635ae079dbbd5c6b26410e5fd3b4
sc78200 <43121353+sc78200@users.noreply.github.com>
2020-01-13 00:35:20 +0100

    Add files via upload

--- src/components/MessageBar.js
+++ src/components/MessageBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
 import { addMessage } from "../action/message";
 import { useDispatch, useSelector } from "react-redux";
 
