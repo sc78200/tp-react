49c995c5cb0fb7512d58089851df0ec67bd91d67
sc78200 <43121353+sc78200@users.noreply.github.com>
2020-01-13 00:34:50 +0100

    Add files via upload

--- src/action/message.js
+++ src/action/message.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { ADD_MESSAGE_ACTION, REMOVE_MESSAGE_ACTION } from '../constants/action';
+import { ADD_MESSAGE_ACTION, REMOVE_MESSAGE_ACTION } from "../constants/action";
 
 export const addMessage = (message, username) => {
   return {
     type: ADD_MESSAGE_ACTION,
     message,
-    username,
+    username
   };
 };
 
 export const removeMessage = index => {
   return {
     type: REMOVE_MESSAGE_ACTION,
-    index,
+    index
   };
 };
